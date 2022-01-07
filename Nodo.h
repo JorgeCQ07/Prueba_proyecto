1a26cb4d617958cddfddc64bc2c29cb723f666b2
JorgeCQ07 <73572833+JorgeCQ07@users.noreply.github.com>
2022-01-07 17:51:24 -0600

    Initial commit

---
+++ Nodo.h
@@ -0,0 +1,23 @@
+//
+// Created by jartu on 1/4/2022.
+//
+
+#ifndef ARBOL_NODO_H
+#define ARBOL_NODO_H
+
+#include <iostream>
+using namespace std;
+
+template <class tipo>
+class Nodo {
+private:
+    tipo* data;
+    Nodo<tipo>* left;
+    Nodo<tipo>* right;
+
+public:
+
+};
+
+
+#endif //ARBOL_NODO_H
